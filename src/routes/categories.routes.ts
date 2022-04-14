127fd9361c994c93407627b01260ecef473b454e
Paulo Abreu <micaeler00@gmail.com>
2022-04-14 14:50:38 -0300

    feat: config multer

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -5,7 +5,9 @@ import { listCategoriesController } from "../modules/cars/usecases/listCategorie
 
 const categoriesRoutes = Router();
 
-const upload = multer({});
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
@@ -15,6 +17,11 @@ categoriesRoutes.get("/", (req, res) => {
   return listCategoriesController.handle(req, res);
 });
 
-categoriesRoutes.post("/import", (req, res) => {});
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  const { file } = req;
+
+  console.log(file);
+  return res.send();
+});
 
 export { categoriesRoutes };
