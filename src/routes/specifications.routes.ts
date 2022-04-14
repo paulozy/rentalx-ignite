ad4af3d253875313e6743981c021c16da1df9f5c
Paulo Abreu <micaeler00@gmail.com>
2022-04-14 14:31:01 -0300

    refactor: move specifications repository to usecases dir

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,18 +1,8 @@
 import { Router } from "express";
-import { SpecificationRepository } from "../modules/cars/repositories/implementations/SpecificationRepository";
-import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+import { createSpecificationController } from "../modules/cars/usecases/createSpecification";
 
 export const specificationsRoutes = Router();
-const specificationRepository = new SpecificationRepository();
 
 specificationsRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationRepository
-  );
-
-  createSpecificationService.execute({ name, description });
-
-  return res.status(201).send();
+  return createSpecificationController.handle(req, res);
 });
