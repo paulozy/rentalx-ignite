118fd325ec8b4c45a1affe279d4918b18025817a
Paulo Abreu <micaeler00@gmail.com>
2022-04-15 00:58:07 -0300

    feat: config swagger server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(router);
 
 app.listen(3001, () => console.log("Server is running"));
